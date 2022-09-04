2f9ecc45aa38d853d86041d7b296f69092c1768e
ItotiaHarrison <harrison.itotia@student.moringaschool.com>
2022-09-04 14:47:03 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,56 +1,53 @@
 // write your code here
 
-const baseUrl = ' http://localhost:3000/ramens'
-
 document.addEventListener('DOMContentLoaded', () => {
 
-function fetchData(){    
-fetch(baseUrl)
-.then(response => response.json())
-.then(ramenData => ramenData.forEach(data => appendData(data))) 
-}
-fetchData()
+    function fetchData() {
+        fetch('http://localhost:3000/ramens')
+            .then(response => response.json())
+            .then(ramenData => ramenData.forEach(data => appendData(data)))
+    }
+    fetchData()
 
-const appendData = (data) => {
-    const imgDiv = document.getElementById("ramen-menu")
-    const img = document.createElement("img")
-    img.src = data.image
-    imgDiv.appendChild(img)
-    img.addEventListener("click", () => updateProfile(data))
+    const appendData = (data) => {
+        const imgDiv = document.getElementById("ramen-menu")
+        const img = document.createElement("img")
+        img.src = data.image
+        imgDiv.appendChild(img)
+        img.addEventListener("click", () => updateProfile(data))
 
 
 
-}
+    }
 
-const updateProfile = (data) => {
-    const ramDetails = document.getElementById("ramen-detail")
-    ramDetails.innerHTML = `<img class="detail-image" src="${data.image}" alt="${data.name}" />
+    const updateProfile = (data) => {
+        const ramenDetails = document.getElementById("ramen-detail")
+        ramenDetails.innerHTML = `<img class="detail-image" src="${data.image}" alt="${data.name}" />
                               <h2 class="name">${data.name}</h2>
                               <h3 class="restaurant">${data.restaurant}</h3>`
 
-const ratings = document.getElementById("rating-display")
-ratings.innerText = data.rating 
-const comments = document.getElementById("comment-display")
-comments.innerText = data.comment
-} 
+        const rating = document.getElementById("rating-display")
+        rating.innerText = data.rating
+        const comment = document.getElementById("comment-display")
+        comment.innerText = data.comment
+    }
 
-function updateFromForm(){
-const form = document.getElementById("new-ramen")
-console.log(form)
-form.addEventListener("submit", (e) => {
-    e.preventDefault()   
-    const name = form.name.value
-    const restaurant = form.restaurant.value
-    const rating = form.rating.value
-    const image  = form.image.value
-    console.log(image)
+    function updateFromForm() {
+        const form = document.getElementById("new-ramen")
+        form.addEventListener("submit", (e) => {
+            e.preventDefault()
+            const name = form.name.value
+            const restaurant = form.restaurant.value
+            const rating = form.rating.value
+            const image = form.image.value
+            console.log(image)
 
-    (document.getElementById("ramen-menu").innerHTML += `<img src="${image}"/>`).addEventListener("click", () => updateProfile(data))
+                (document.getElementById("ramen-menu").innerHTML += `<img src="${image}"/>`).addEventListener("click", () => updateProfile(data))
 
-    console.log(name)
+            console.log(name)
 
-})
-}
-updateFromForm()
+        })
+    }
+    updateFromForm()
 
 })
